f9c89bacab756bd34fbfca7fb1429e1e7e2d50be
UnstableLuca <lucas.joglar@gmail.com>
2024-11-26 14:34:43 +0100

    Primera Build Funcional

--- GameOfLife.js
+++ GameOfLife.js
@@ -1,11 +1,11 @@
-var rows = 900;
-var col = 500;
+const width = 900;
+const height = 500;
 
 class Cell
 {
     constructor(isAlive = false)
     {
-        this.state = isAlive;
+        this.isAlive = isAlive;
         this.timeAlive = 0;
         this.timeDead = 0;
     }
@@ -28,17 +28,19 @@ class Cell
 
     updateTimer()
     {
-        if(isAlive)
+        if(this.isAlive)
             {
-                this.timeAlive =+ 1;
+                this.timeAlive += 1;
+                this.timeDead = 0;
             } else {
-                this.timeDead =+ 1;
+                this.timeDead += 1;
+                this.timeAlive = 0;
             }
     }
 
     changeState()
     {
-        this.state = !this.state;
+        this.isAlive = !this.isAlive;
         this.timeAlive = 0;
         this.timeDead = 0;
     }
@@ -48,18 +50,42 @@ class Cell
 
 class Mundo 
 {
-    constructor(rows , cols)
+    constructor(rows = 50, cols = 50, fps = 10)
     {
         this.rows = rows;
         this.cols = cols;
         this.matrix = this.createWorld();
-        this.canvas = document.getElementById('canvasCentral');
-        this.ctx = canvas.getContext('2d');
-        this.cellSize = Math.min(this.canvas.width / this.cols , this.canvas.height / this.rows );
-        this.canvas.width = this.cols * this.cellSize;
-        this.canvas.height = this.rows * this.cellSize;
+        this.showGrid = true;
+        this.isRunning = false;
+        this.timer = null;
+        this.fps = fps;
 
-        this.canvas.addEventListener('click', (e) => this.toggleCellState(e));
+        this.canvas = document.getElementById("canvasCentral");
+        this.ctx = this.canvas.getContext("2d");
+
+        this.canvas.width = width; 
+        this.canvas.height = height;
+
+        this.cellWidth = this.canvas.width / this.cols;
+        this.cellHeight = this.canvas.height / this.rows;
+
+        this.canvas.addEventListener("click", (e) => this.toggleCellState(e));
+        this.canvas.addEventListener("mousemove", (e) => this.showTime(e));
+
+        const playBtn = document.getElementById("Jugar");
+        playBtn.addEventListener("click", () => this.togglePlay());
+
+        const restartBtn = document.getElementById("Reiniciar");
+        restartBtn.addEventListener("click", () => this.clearWorld());
+
+        const nextBtn = document.getElementById("Siguiente");
+        nextBtn.addEventListener("click", () => this.nextIteration());
+
+        const gridToggleCheckbox = document.getElementById("toggleGrid");
+        gridToggleCheckbox.addEventListener("change", () => this.toggleGrid());
+
+        const fpsSlider = document.getElementById("fpsSlider");
+        fpsSlider.addEventListener("input", (e) => this.updateFPS(e));
     }
 
 
@@ -68,12 +94,12 @@ class Mundo
         const matrix = [];
         for (let i = 0; i < this.rows; i++)
         {
-            const lines = [];
+            const row = [];
             for (let j = 0; j < this.cols; j++)
             {
-                lines.push(new Cell(false));
+                row.push(new Cell(false));
             }
-            matrix.push(line);
+            matrix.push(row);
         }
         return matrix;
     }
@@ -81,49 +107,162 @@ class Mundo
     paintWorld()
     {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
             for (let i = 0; i < this.rows; i++) {
                 for (let j = 0; j < this.cols; j++) {
                     const cell  = this.matrix[i][j];
-                    if(cell.state){
+                    if(cell.isAlive){
                         this.ctx.fillStyle = '#FFFFFF';
                     } else {
                         this.ctx.fillStyle = '#000F08';
                     }
-                    this.ctx.fillRect(j * this.cellSize, i * this.cellSize, this.cellSize, this.cellSize);
-                    this.ctx.strokeStyle = 'black';
-                    this.ctx.strokeRect(j * this.cellSize, i * this.cellSize, this.cellSize, this.cellSize); 
+                    this.ctx.fillRect(
+                        j * this.cellWidth,
+                        i * this.cellHeight,
+                        this.cellWidth,
+                        this.cellHeight
+                    );
                 }
             }
+
+            this.ctx.strokeStyle = 'black';
+            this.ctx.lineWidth = 1;
+        
+            //GRID
+            if(this.showGrid){
+            for (let i = 0; i <= this.cols; i++) {
+                this.ctx.beginPath();
+                this.ctx.moveTo(i * this.cellWidth, 0);
+                this.ctx.lineTo(i * this.cellWidth, this.canvas.height);
+                this.ctx.stroke();
+            }
+        
+
+            for (let j = 0; j <= this.rows; j++) {
+                this.ctx.beginPath();
+                this.ctx.moveTo(0, j * this.cellHeight);
+                this.ctx.lineTo(this.canvas.width, j * this.cellHeight);
+                this.ctx.stroke();
+            }
+        }
+    }
+
+    clearWorld()
+    {
+        this.matrix = this.createWorld();
+        this.paintWorld();
+        if(this.isRunning)
+        {
+            this.togglePlay();
+        }
+    }
+
+    nextIteration()
+    {
+        if(!this.isRunning)
+        {
+            this.updateWorld();
+        }
     }
 
     toggleCellState(event)
     {
         const rect = this.canvas.getBoundingClientRect();
-        const x = Math.floor((event.clientY - rect.top) / this.cellSize);
-        const y = Math.floor((event.clientX - rect.left) / this.cellSize);
-        this.matrix[x][y].changeState();
+        const x = Math.floor((event.clientY - rect.top) / this.cellHeight);
+        const y = Math.floor((event.clientX - rect.left) / this.cellWidth);
 
+        this.matrix[x][y].changeState();
+        this.paintWorld();
+    }
+
+    getAliveNeighbours(x,y)
+    {
+        let alive = 0;
+        for (let i = -1; i <=  1; i++) {
+            for (let j = -1; j <= 1; j++) {
+                if(i == 0 && j == 0)
+                {
+                    continue;
+                }
+
+                const newX = x + i ;
+                const newY = y + j ;
+
+                if (newX >= 0 && newX < this.rows && newY >= 0 && newY < this.cols) {
+                    if (this.matrix[newX][newY].isAlive) {
+                        alive++;
+                    }
+                }
+            }
+        }
+        return alive;
+    }
+
+    updateFPS(event)
+    {
+        this.fps = parseInt(event.target.value);
+        document.getElementById("fpsValue").textContent = this.fps;
+
+        if (this.isRunning) {
+            clearInterval(this.timer);
+            this.timer = setInterval(() => this.updateWorld(), 1000 / this.fps);
+        }
+    }
+
+
+    updateWorld()
+    {
+        const newStates = this.matrix.map((row, i) => 
+            row.map((cell, j) => {
+              const aliveNeighbours = this.getAliveNeighbours(i, j);
+              const newCell = new Cell(cell.isAlive); 
+              newCell.update(aliveNeighbours); 
+              return newCell; 
+            })
+          );
+        this.matrix = newStates;
+        this.paintWorld();
+    }
+
+    togglePlay()
+    {
+        const button = document.getElementById("Jugar")
+        if(this.isRunning)
+        {
+            clearInterval(this.timer);
+            this.isRunning = false;
+            button.textContent = "Jugar";
+        } else {
+            this.timer = setInterval(() => this.updateWorld(), 1000 / this.fps);
+            this.isRunning = true;
+            button.textContent = "Pausar";
+        }
+    }
+
+    showTime(event)
+    {
+        const rect = this.canvas.getBoundingClientRect();
+        const x = Math.floor((event.clientY - rect.top) / this.cellHeight);
+        const y = Math.floor((event.clientX - rect.left) / this.cellWidth);
+
+        const cell = this.matrix[x][y];
+        const textoOut = document.getElementById("tiempo");
+
+        if(cell.isAlive)
+        {
+            timeDisplay.textContent = `Time Alive: ${cell.timeAlive}`;
+        } else {
+            timeDisplay.textContent = `Time Alive: ${cell.timeDead}`;
+        }
+    }
+
+    toggleGrid()
+    {
+        const gridCheck = document.getElementById("toggleGrid");
+        this.showGrid = gridCheck.checked;
         this.paintWorld();
     }
 }
 
-canvas.width = rows; 
-canvas.height = cols; 
-ctx.fillStyle = '#000F08';
-ctx.fillRect(0, 0, canvas.width, canvas.height);
-ctx.strokeStyle = '#FFFFFF';
-ctx.lineWidth = 1;
-
-for (let x = 0; x <= canvas.width; x += 25) {
-    ctx.beginPath();
-    ctx.moveTo(x, 0);
-    ctx.lineTo(x, canvas.height);
-    ctx.stroke();
-}
-
-for (let y = 0; y <= canvas.height; y += 25) {
-    ctx.beginPath();
-    ctx.moveTo(0, y);
-    ctx.lineTo(canvas.width, y);
-    ctx.stroke();
-}
+const world = new Mundo();
+world.paintWorld();
